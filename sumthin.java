2b20a32dc88ab1cb0097625cf4b9f188239afa04
hayd6262 <hayd6262@gmail.com>
2022-01-25 09:45:31 -0500

    i was bored. i think ill start using this repo as a collection of java files i felt like making but i might make branches of helpful stuff

--- sumthin.java
+++ sumthin.java
@@ -2,6 +2,27 @@
  * sumthin
  */
 public class sumthin {
-//iouno wut im doin here
+    String boredom;
     
+    
+    public sumthin() {// constructor
+        
+        
+    }
+    public void uBored(boolean isBored) {//made this while i was bored to check if im bored
+        
+        if (isBored == true) {
+            System.out.println("well that sucks");
+        } else {
+            System.out.println("kool");
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        sumthin amBored = new sumthin();
+        amBored.uBored(true);
+        
+    }
 }
